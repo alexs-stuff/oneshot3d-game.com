a790e6fa51e9937922f2787b01730c7b72937b09
al_ex427 <alexsroblox123@gmail.com>
2026-04-04 22:36:36 +0300

    russian

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,7 +3,7 @@ import type { AstroI18nextConfig } from "astro-i18next";
 
 const config: AstroI18nextConfig = {
   defaultLocale: "en",
-  locales: ["en", "ro"],
+  locales: ["en", "ro", "ru"],
 };
 
 export default config;
