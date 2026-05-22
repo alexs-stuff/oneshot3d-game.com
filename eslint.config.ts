784cd6aab0ebe5074dcad5c0bac18f11ae8df0b8
al_ex427 <alexsroblox123@gmail.com>
2026-05-22 15:44:41 +0300

    react

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,13 +3,25 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
+import { globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-plugin-prettier";
 export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
+    plugins: { prettier, js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   tseslint.configs.recommended,
 ]);
